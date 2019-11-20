fdefeae2858c6b3c4b78d7c69ad4b6c39adc3414
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-20 23:21:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,7 +21,7 @@ class String
     #   split_sentence << self.split("?").uniq!
     #   split_sentence << self.split("!").uniq!
     #   split_sentence.count
-     self.split(/[.!?]+(?=\s|\z)//).count
+     self.scan(/[.!?]+(?=\s|\z)//).count
 
 
   end
